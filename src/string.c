f67f65df69ace8763deaa7f357437c13431f10ec
Hsu <edittye379@gmail.com>
2017-10-26 22:49:01 +0800

    C99...

--- src/string.c
+++ src/string.c
@@ -2,7 +2,8 @@
 #include "string.h"
 
 void str_trim_lf (char* arr, int length) {
-    for (int i = 0; i < length; i++) { // trim \n
+    int i;
+    for (i = 0; i < length; i++) { // trim \n
         if (arr[i] == '\n') {
             arr[i] = '\0';
             break;
